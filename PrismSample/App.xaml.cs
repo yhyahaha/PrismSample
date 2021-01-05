e9cdd6a379cdebf62b950c5559700aa1ebd4f5ea
yhyahaha <yhyahaha@live.jp>
2021-01-05 20:08:01 +0900

    Application_StartUp, Exit
    Mutex 二重起動防止

--- PrismSample/App.xaml.cs
+++ PrismSample/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Prism.Ioc;
 using PrismSample.Views;
+using System.Threading;
 using System.Windows;
 
 namespace PrismSample
@@ -9,14 +10,38 @@ namespace PrismSample
     /// </summary>
     public partial class App
     {
+        private Mutex mutex = new Mutex(false, "yhyahahaPrismSample");
+
         protected override Window CreateShell()
         {
             return Container.Resolve<MainWindow>();
         }
 
+
+
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
 
+        }
+
+        private void PrismApplication_Startup(object sender, StartupEventArgs e)
+        {
+            if (this.mutex.WaitOne(0, false)) return;
+
+            MessageBox.Show("二重起動できません。", "情報", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            this.mutex.Close();
+            this.mutex = null;
+            this.Shutdown();
+        }
+
+        private void PrismApplication_Exit(object sender, ExitEventArgs e)
+        {
+            if (this.mutex != null)
+            {
+                this.mutex.ReleaseMutex();
+                this.mutex.Close();
+            }
         }
     }
 }
