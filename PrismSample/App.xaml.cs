effe907fa92115a8112fac4b9fda50fa38cde579
yhyahaha <yhyahaha@live.jp>
2021-01-05 20:01:09 +0900

    プロジェクト ファイルを追加します。

---
+++ PrismSample/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using PrismSample.Views;
+using System.Windows;
+
+namespace PrismSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
