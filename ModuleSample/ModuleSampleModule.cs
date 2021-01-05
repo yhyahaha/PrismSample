ed7d65e1cd9b43dd4693d4891ee035d772ff9ca3
yhyahaha <yhyahaha@live.jp>
2021-01-05 20:20:24 +0900

    ModuleSampleを追加

---
+++ ModuleSample/ModuleSampleModule.cs
@@ -0,0 +1,20 @@
+﻿using ModuleSample.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ModuleSample
+{
+    public class ModuleSampleModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
