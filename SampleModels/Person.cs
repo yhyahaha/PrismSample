5150d9bfcc36ffdc3583aae312e4d90660db69f8
yhyahaha <yhyahaha@live.jp>
2021-01-06 16:55:54 +0900

    SampleModels 追加

---
+++ SampleModels/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrismSample
+{
+    public class Person
+    {
+		public int Id { get; set; } = 0;
+
+		public string Name { get; set; } = string.Empty;
+
+		public string Kana { get; set; } = string.Empty;
+
+		public DateTime? BirthDay { get; set; } = null;
+
+		public int? Sex { get; set; } = null;
+	}
+}
